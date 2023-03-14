14c04d33f7495095349ca00e547ef5ecfdaf1ef3
Vaibhav <vaibhavovhal15@gmail.com>
2023-03-14 20:59:42 +0530

    [Vaibhav] Add:Position After Die Roll

--- Snake-Ladder/Play.cs
+++ Snake-Ladder/Play.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,13 +11,15 @@ namespace Snake_Ladder
     {
         const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POSITION = 100;
         //UC1
-        int playerposition = 0;
+        int playerposition = 0 , count = 0;
 
         //UC2
         Random random = new Random();
         public int DieRoll()
         {
             int num = random.Next(1, 7);
+            Console.WriteLine("Die Value ----->    " + num);
+            count++;
             return num;
         }
         public void Game()
@@ -44,6 +47,7 @@ namespace Snake_Ladder
             if (this.playerposition == WINNING_POSITION)
             {
                 Console.WriteLine("Player position ----->   " + playerposition);
+                Console.WriteLine("Die Count ---->     " + count);
             }
         } 
     }
