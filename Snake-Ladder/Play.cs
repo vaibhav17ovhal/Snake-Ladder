bb2de03b00ed8db2943136c8e9e3e5bd854e8353
Vaibhav <vaibhavovhal15@gmail.com>
2023-03-14 19:43:08 +0530

    [Vaibhav] Add:Welcome to the game

---
+++ Snake-Ladder/Play.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Ladder
+{
+    public class Play
+    {
+        //UC1
+        int playerposition = 0;
+    }
+}
---
+++ Snake-Ladder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeLadder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and ladder game");
+        }
+    }
+}
+    
